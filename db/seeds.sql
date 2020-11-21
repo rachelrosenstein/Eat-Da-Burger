f6a49490a1ef8de486ff025827f69fac583f3771
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-11-21 07:37:05 -0500

    config set up

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', FALSE);
