be86f0b088f9caa14de4b327cc38d9af8030c49a
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-11-23 20:04:46 -0500

    Names show up on the page

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', FALSE);
+INSERT INTO burgers (name) VALUES ('Cheeseburger Classic');
+INSERT INTO burgers (name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Bacon Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Southwest Burger', true);
+INSERT INTO burgers (name) VALUES ('Joe Biden Burger');
